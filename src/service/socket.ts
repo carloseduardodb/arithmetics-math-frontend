86f121090e9dfa8336df87377318ef3ad13c5959
carloseduardodb <carloscangere@hotmail.com>
2021-04-19 18:34:30 -0300

    update to deploy

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:3001");
-//export const socket = io("https://server-arithmetics-math.herokuapp.com");
+//export const socket = io("http://localhost:3001");
+export const socket = io(
+  "https://server-arithmetics-math.herokuapp.com/:53445"
+);
